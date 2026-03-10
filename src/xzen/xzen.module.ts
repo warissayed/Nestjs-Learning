91dc9cf4694d2807c253bda328376b61383065e7
warissayed <warissayed72@gmail.com>
2026-03-10 12:01:38 -0700

    feat: restart the learning day 0 implemented

--- src/xzen/xzen.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { XzenController } from './xzen.controller';
-import { XzenService } from './xzen.service';
-
-@Module({
-  controllers: [XzenController],
-  providers: [XzenService]
-})
-export class XzenModule {}
