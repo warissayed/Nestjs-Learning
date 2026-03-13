c6df93a5d8b437ec9782d2239c6b4fad5ff7cf89
warissayed <warissayed72@gmail.com>
2026-03-12 18:14:26 -0700

    frat: updated the api layout map

--- lesson1/src/user/user.controller.ts
+++ lesson1/src/user/user.controller.ts
@@ -2,6 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
+  /* 
+  GET /Users/:id
+  POST /Users
+  PATCH /Users/:id
+  DELETE /Users/:id
+  
+  */
   @Get()
   findAll(): string {
     return 'This action returns all cats';
