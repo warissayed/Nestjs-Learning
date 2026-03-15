50866b94845e47d8604a7b250d0e7726f45516cd
warissayed <warissayed72@gmail.com>
2026-03-15 23:53:13 +0530

    feat : added services for doctor

--- lesson1/src/app.module.ts
+++ lesson1/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DoctorController } from './doctor/doctor.controller';
+import { DoctorService } from './doctor/doctor.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController, DoctorController],
-  providers: [AppService],
+  providers: [AppService, DoctorService],
 })
 export class AppModule {}
