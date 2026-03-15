dc1faa5cae0c13ae895ce093717c33a9e384ff1d
warissayed <warissayed72@gmail.com>
2026-03-15 23:52:33 +0530

    feat:added doctor  controller

--- lesson1/src/app.module.ts
+++ lesson1/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DoctorController } from './doctor/doctor.controller';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController],
+  controllers: [AppController, DoctorController],
   providers: [AppService],
 })
 export class AppModule {}
