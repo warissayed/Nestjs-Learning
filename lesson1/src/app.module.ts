e289e0bbc7e360a7a71eea49718f9e3300c50d6f
warissayed <warissayed72@gmail.com>
2026-03-19 08:33:47 +0530

    feat: Restarting the project for better understanding

--- lesson1/src/app.module.ts
+++ lesson1/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { DoctorController } from './doctor/doctor.controller';
-import { DoctorService } from './doctor/doctor.service';
-import { DoctorModule } from './doctor/doctor.module';
 
 @Module({
-  imports: [UserModule, DoctorModule],
-  controllers: [AppController, DoctorController],
-  providers: [AppService, DoctorService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
