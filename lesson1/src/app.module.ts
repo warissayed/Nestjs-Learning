32da60a141e0bd44ac34e5d35fd7c3ac3cc94b80
warissayed <warissayed72@gmail.com>
2026-04-11 22:37:55 +0530

    feat: adding mongodb

--- lesson1/src/app.module.ts
+++ lesson1/src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.development', '.env.production'],
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
   ],
   controllers: [AppController],
