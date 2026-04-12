1bfa56c348167ea599d12368186c27a3a86717c6
warissayed <warissayed72@gmail.com>
2026-04-13 01:30:41 +0530

    feat: updated user types

---
+++ lesson1/src/user/Schemas/User.Schemas.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  fname: string;
+
+  @Prop({ required: true })
+  lname: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: 'user' })
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
