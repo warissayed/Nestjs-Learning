3c59a697880d0023cfc3e4b76326e39bc455824f
warissayed <warissayed72@gmail.com>
2026-06-08 22:48:32 +1000

    feat: Edit name

--- lesson1/src/app.service.ts
+++ lesson1/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!Lest start with NestJS';
+    return 'Hello World!';
   }
 }
